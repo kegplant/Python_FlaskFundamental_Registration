9ffc16af7f6bba3d80468a89caa7eeefe116eafa
kegplant <ace1905@gmail.com>
2017-11-04 16:40:34 -0700

    basic features complete

--- registration.py
+++ registration.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect,session, flash
 import random
+import re
 app = Flask(__name__)
 app.secret_key='poshit'
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 # our index route will handle rendering our form
 @app.route('/')
 def index():
@@ -9,11 +11,28 @@ def index():
 @app.route('/process', methods=['POST'])
 def process():
     session['user']=request.form
-    if False:
-        flash('')
+    for key,value in request.form.items():
+        if len(request.form[key])<1:
+            flash('{} must not be empty!'.format(key))
+            return redirect('/')
+    if not (request.form['first_name'].isalpha() and request.form['last_name'].isalpha()):
+        flash('First and Last Names are letters only!')
         return redirect('/')
-    flash('Thanks for submitting your information')
-    return render_template('result.html',info=session['user'])
+    if len(request.form['password'])<9:
+        flash('Password must be 8 or more letters long!')
+        return redirect('/')
+    if len(request.form['password'])!=len(request.form['password2']):
+        flash('Passwords must match!')
+        return redirect('/')
+    if not EMAIL_REGEX.match(request.form['eml']):
+        flash('Invalid Email Address!')
+        return redirect('/')
+    flash('Thanks for submitting your information!')
+    return redirect('/')
+    # print type(request.form)
+    # print 'abcde'
+
+    # return render_template('result.html',info=session['user'])
 
 
 app.run(debug=True)
