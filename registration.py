7ab13c40599bce8dfe204cf9629e61f4781929e8
kegplant <ace1905@gmail.com>
2017-11-04 17:53:01 -0700

    feature complete

--- registration.py
+++ registration.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect,session, flash
 import random
 import re
+import datetime
 app = Flask(__name__)
 app.secret_key='poshit'
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
@@ -18,7 +19,7 @@ def process():
     if not (request.form['first_name'].isalpha() and request.form['last_name'].isalpha()):
         flash('First and Last Names are letters only!')
         return redirect('/')
-    if len(request.form['password'])<9:
+    if len(request.form['password'])<8:
         flash('Password must be 8 or more letters long!')
         return redirect('/')
     if len(request.form['password'])!=len(request.form['password2']):
@@ -26,6 +27,13 @@ def process():
         return redirect('/')
     if not EMAIL_REGEX.match(request.form['eml']):
         flash('Invalid Email Address!')
+        return redirect('/')
+    if (not any(c.isupper() for c in request.form['password'])) or (not any(c.isdigit() for c in request.form['password'])):
+        flash('Password must have at least one capital letter and one number!')
+        return redirect('/')
+    today=str(datetime.date.today())
+    if (today<request.form['bday']):
+        flash('B-day must not be in the future!')
         return redirect('/')
     flash('Thanks for submitting your information!')
     return redirect('/')
